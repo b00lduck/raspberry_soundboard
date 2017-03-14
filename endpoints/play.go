f3c8118d0c5d174d34ddb82b8512499c04994e13
Salty Blu <s.blu24@gmail.com>
2017-03-14 14:07:43 +0100

    add omxyoutube
    needs a script like this:
    ---
    export DISPLAY=:0
    
    /usr/bin/omxplayer `youtube-dl -g $1`
    ---

--- endpoints/play.go
+++ endpoints/play.go
@@ -1,17 +1,19 @@
 package endpoints
 
 import (
-	"strings"
-	"os"
 	"fmt"
-	"os/exec"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/b00lduck/raspberry_soundboard/persistence"
 )
 
 func InitPlay(persistence *persistence.Persistence) {
-	http.HandleFunc("/api/play/", func (w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/play/", func(w http.ResponseWriter, r *http.Request) {
 
 		log.WithField("URI", r.RequestURI).Info("Incoming play request")
 
@@ -53,6 +55,30 @@ func playSound(filename string, persistence *persistence.Persistence) error {
 			persistence.IncCounter(filename)
 
 		}
+
+	} else if strings.HasSuffix(filename, ".youtube") {
+		cat := persistence.GetCategory(filename)
+
+		filenameWithPath := "sounds/" + cat + "/" + filename
+		log.WithField("filename", filename).Info("playing video")
+		if _, err := os.Stat(filenameWithPath); os.IsNotExist(err) {
+			log.Error(filenameWithPath)
+			return fmt.Errorf("Not found")
+		}
+
+		b, _ := ioutil.ReadFile(filenameWithPath)
+		url := string(b)
+		url := strings.TrimSpace(url)
+
+		go func() {
+			cmd := exec.Command("omxyoutube", url)
+			err := cmd.Run()
+			if err != nil {
+				log.Error(err)
+			}
+		}()
+
+		persistence.IncCounter(filename)
 
 	} else {
 		return fmt.Errorf("no .mp3 suffix")
